386a753683e70a89727d920b66b566bdfcde45d1
Edidiong Ndaobong <edidiong.ndaobong@maxdrive.ai>
2024-09-14 15:56:03 +0100

    fix

--- api/contact.js
+++ api/contact.js
@@ -14,17 +14,9 @@ const contactEmail = nodemailer.createTransport({
   },
 });
 
-contactEmail.verify((error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Ready to Send");
-  }
-});
-
-console.log("GMAIL_USER:", process.env.GMAIL_USER);
 
 export default async function handler(req, res) {
+console.log("GMAIL_USER:", process.env.GMAIL_USER);
   if (req.method === 'POST') {
     const { firstName, lastName, email, message, phone } = req.body;
     const name = `${firstName} ${lastName}`;
