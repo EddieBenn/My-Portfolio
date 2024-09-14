b4a321a3f072b982c8e14ef2ecbf4d694e24678c
Edidiong Ndaobong <edidiong.ndaobong@maxdrive.ai>
2024-09-14 15:00:08 +0100

    fix

--- src/Axios/apiCall.ts
+++ src/Axios/apiCall.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const sendMessage = async(body:any)=>{
     try{
-      const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+      const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || `https://edidiong-benn-portfolio.vercel.app`;
       console.log(API_BASE_URL)
       const response = await axios.post(`${API_BASE_URL}/contact`, body,{
         headers: {
